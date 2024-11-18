78f9c1eefd5b59fd36d0788cc0c05464f4d19ac1
Salzii12 <syalziastaudi12@gmail.com>
2024-11-18 15:43:07 +0700

    Merapikan ContainerBox.kt

--- app/src/main/java/com/app/amarine/ui/screen/profile/ProfileScreen.kt
+++ app/src/main/java/com/app/amarine/ui/screen/profile/ProfileScreen.kt
@@ -1,5 +1,6 @@
 package com.app.amarine.ui.screen.profile
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,10 +10,12 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material.icons.outlined.Logout
 import androidx.compose.material.icons.outlined.ModeEdit
 import androidx.compose.material.icons.outlined.Security
 import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material.icons.outlined.ShoppingBag
 import androidx.compose.material.icons.rounded.ArrowBackIosNew
 import androidx.compose.material.icons.rounded.KeyboardArrowRight
 import androidx.compose.material3.HorizontalDivider
@@ -23,6 +26,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -113,6 +117,11 @@ fun ProfileContent(
                             imageVector = Icons.Rounded.KeyboardArrowRight,
                             contentDescription = null,
                         )
+//                        Icon(
+//                            imageVector = if (user.isKtpVerified) Icons.Filled.CheckCircle else Icons.Filled.Cancel,
+//                            contentDescription = null,
+//                            tint = if (user.isKtpVerified) Color.Green else Color.Red
+//                        )
                     },
                     onClick = onEditProfileClick
                 )

e0c6ab597ef3e07fb866305140a2fb5da2e3876f
DhaniDzulkarnain1 <dhanidzulkarnain1@gmail.com>
2024-12-10 15:41:50 +0700

    Update Retrofit Client

--- app/src/main/java/com/app/amarine/RetrofitClient.kt
+++ app/src/main/java/com/app/amarine/RetrofitClient.kt
@@ -1,33 +1,36 @@
-package com.app.amarine
-
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
-
-object RetrofitClient {
-    private const val BASE_URL = "http:192.168.152.51:3000" // sesuaikan dengan URL dan port Anda
-
-    val instance: ApiService by lazy {
-        val loggingInterceptor = HttpLoggingInterceptor().apply {
-            level = HttpLoggingInterceptor.Level.BODY
-        }
-
-        val client = OkHttpClient.Builder()
-            .addInterceptor(loggingInterceptor)
-            .connectTimeout(60, TimeUnit.SECONDS)
-            .readTimeout(60, TimeUnit.SECONDS)
-            .writeTimeout(60, TimeUnit.SECONDS)
-            .build()
-
-        val retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .client(client)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        retrofit.create(ApiService::class.java)
-    }
-}
-
+//package com.app.amarine
+//
+//import okhttp3.OkHttpClient
+//import okhttp3.logging.HttpLoggingInterceptor
+//import retrofit2.Retrofit
+//import retrofit2.converter.gson.GsonConverterFactory
+//import java.util.concurrent.TimeUnit
+//
+//object RetrofitClient {
+//    // Opsi 1: Menggunakan localhost untuk emulator
+//    // private const val BASE_URL = "http://10.0.2.2:3000"  // Hapus slash di akhir
+//
+//    // Opsi 2: Atau gunakan IP yang sama dengan Postman
+//    private const val BASE_URL = "http://10.170.15.114:3000"
+//
+//    val instance: ApiService by lazy {
+//        val loggingInterceptor = HttpLoggingInterceptor().apply {
+//            level = HttpLoggingInterceptor.Level.BODY
+//        }
+//
+//        val client = OkHttpClient.Builder()
+//            .addInterceptor(loggingInterceptor)
+//            .connectTimeout(60, TimeUnit.SECONDS)
+//            .readTimeout(60, TimeUnit.SECONDS)
+//            .writeTimeout(60, TimeUnit.SECONDS)
+//            .build()
+//
+//        val retrofit = Retrofit.Builder()
+//            .baseUrl("$BASE_URL/") // Tambahkan slash di sini
+//            .client(client)
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .build()
+//
+//        retrofit.create(ApiService::class.java)
+//    }
+//}

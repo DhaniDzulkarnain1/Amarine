938341918473d568c18b01f98f70a89ba82b0b83
DhaniDzulkarnain1 <dhanidzulkarnain1@gmail.com>
2024-12-10 15:36:06 +0700

    Hilt Module

---
+++ app/src/main/java/com/app/amarine/HiltModule.kt
@@ -0,0 +1,26 @@
+package com.app.amarine
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object HiltModule {
+
+    @Provides
+    @Singleton
+    fun provideSessionManager(@ApplicationContext context: Context): SessionManager {
+        return SessionManager(context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideUserPreferences(@ApplicationContext context: Context): UserPreferences {
+        return UserPreferences(context)
+    }
+}

792da3937d64ee7090deacc5ec3fea875a134d26
DhaniDzulkarnain1 <dhanidzulkarnain1@gmail.com>
2024-12-10 15:42:44 +0700

    Update User Preferences

--- app/src/main/java/com/app/amarine/UserPreferences.kt
+++ app/src/main/java/com/app/amarine/UserPreferences.kt
@@ -4,25 +4,35 @@ import android.content.Context
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
+import javax.inject.Inject
 
-class UserPreferences(context: Context) {
+class UserPreferences @Inject constructor(context: Context) {
     private val prefs = context.getSharedPreferences("UserPrefs", Context.MODE_PRIVATE)
 
-    fun saveUser(id: Int, email: String, nama: String) {
+    fun saveUser(id: Int, email: String, nama: String, nelayanId: Int) {
         prefs.edit().apply {
             putInt("USER_ID", id)
             putString("USER_EMAIL", email)
             putString("USER_NAMA", nama)
+            putInt("NELAYAN_ID", nelayanId)
             apply()
         }
+    }
+
+    fun getUserId(): Int {
+        return prefs.getInt("USER_ID", 0)
+    }
+
+    fun getUserEmail(): String {
+        return prefs.getString("USER_EMAIL", "") ?: ""
     }
 
     fun getUserNama(): String {
         return prefs.getString("USER_NAMA", "Guest") ?: "Guest"
     }
 
-    fun getUserEmail(): String {
-        return prefs.getString("USER_EMAIL", "") ?: ""
+    fun getNelayanId(): Int {
+        return prefs.getInt("NELAYAN_ID", 0)
     }
 
     fun clearUser() {

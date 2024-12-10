1642165713afad62c26c9b56484d2b2861de7142
DhaniDzulkarnain1 <dhanidzulkarnain1@gmail.com>
2024-12-10 15:33:52 +0700

    CRUD Route Pencatatan

--- app/src/main/java/com/app/amarine/ui/screen/detail_note/DetailNoteScreenRoute.kt
+++ app/src/main/java/com/app/amarine/ui/screen/detail_note/DetailNoteScreenRoute.kt
@@ -5,16 +5,21 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
-import com.app.amarine.model.notes
 import com.app.amarine.ui.navigation.Screen
 
 fun NavGraphBuilder.detailNoteScreenRoute(navController: NavController) {
     composable(
         route = Screen.DetailNote.route,
-        arguments = listOf(navArgument("noteId") { type = NavType.IntType })
+        arguments = listOf(
+            navArgument("noteId") {
+                type = NavType.IntType
+            }
+        )
     ) { backStackEntry ->
         val noteId = backStackEntry.arguments?.getInt("noteId") ?: 0
-        val note = notes.find { it.id == noteId }
-        DetailNoteScreen(note = note, navController = navController)
+        DetailNoteScreen(
+            noteId = noteId,
+            navController = navController
+        )
     }
 }
